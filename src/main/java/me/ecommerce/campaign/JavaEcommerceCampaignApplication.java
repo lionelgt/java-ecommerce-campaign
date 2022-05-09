715e03fd3a8108b036c0bc5f4f65b8403f92f817
lionelgt <lionel.gonzalest@gmail.com>
2022-05-08 20:14:37 -0500

    feat: Baseline of campaign microservice for an e-commerce

---
+++ src/main/java/me/ecommerce/campaign/JavaEcommerceCampaignApplication.java
@@ -0,0 +1,13 @@
+package me.ecommerce.campaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEcommerceCampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEcommerceCampaignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ecommerce/campaign/JavaEcommerceCampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ecommerce.campaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEcommerceCampaignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
